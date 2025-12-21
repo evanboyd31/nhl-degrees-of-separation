b417b55443c4c8a4197c2fc7656d3b73132c04ba
Evan Boyd <evan.boyd@mail.mcgill.ca>
2025-12-21 14:49:05 -0800

    Update get_shortest_path service function to include headshots of each player in the response

--- backend/app/services/players_service.py
+++ backend/app/services/players_service.py
@@ -27,8 +27,28 @@ def get_shortest_path(session: Session, player_1_id: int, player_2_id: int):
   :type player_2_id: int
   """
 
-  results = run_get_shortest_path_between_two_players(session=session,
-                                                      player_1_id=player_1_id,
-                                                      player_2_id=player_2_id)
+  shortest_path_results = run_get_shortest_path_between_two_players(session=session,
+                                                                    player_1_id=player_1_id,
+                                                                    player_2_id=player_2_id)
+  
+  hops = shortest_path_results[0].get("hops")
+  nodes = shortest_path_results[0].get("node_attrs")
+  relationships = shortest_path_results[0].get("relationship_attrs")
+  response = []
 
-  return {"results": results}
+  # handle case where no path exists
+  if not relationships:
+    return {"results": {"hops": hops, "path": []}}
+
+  # iterate over all nodes and add the most recent headshot to each player in the path
+  for node_index, node in enumerate(nodes):
+    if "position_code" in node:
+      if node_index == 0:
+        relationship = relationships[0]
+      else:
+        relationship = relationships[node_index - 1]
+
+      node["headshot_url"] = relationship.get("headshot_url")
+    response.append(node)
+
+  return {"results": {"hops": hops, "path": response}}
