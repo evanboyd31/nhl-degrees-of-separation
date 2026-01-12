0cb2ff405dced224ab2daf0f0782c53b202ba8fb
Evan Boyd <evan.boyd@mail.mcgill.ca>
2026-01-11 18:41:15 -0800

    Update CORS policy to allow requests from localhost

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import health_check
 from app.api.routes import players
 
 app = FastAPI(title="NHL Degrees of Separation")
+
+# allowed CORS origins
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+)
 
 # health check routes
 app.include_router(health_check.router)
