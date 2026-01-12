13669476563a2b09f28a4bad4852ac14a1c9f17b
Evan Boyd <evan.boyd@mail.mcgill.ca>
2026-01-11 21:00:18 -0800

    Update the App.tsx landing page to display the PlayerSearchHeader and a placeholder body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import PlayerSearchHeader from "./components/PlayerSearchHeader";
+import { getShortestPathBetweenTwoPlayers } from "./api/nhlAPI";
+import { CircularProgress } from "@mui/material";
+import { type Player } from "./types/nhl";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [player1, setPlayer1] = useState<Player | null>(null);
+  const [player2, setPlayer2] = useState<Player | null>(null);
+  const [shortestPath, setShortestPath] = useState([]);
+  const [loadingPath, setLoadingPath] = useState<Boolean>(false);
+
+  useEffect(() => {
+    /**
+     * The getShortestPath function will update the displayed shortest path everytime
+     * two new players (player1 and player2 state vars) are selected in the dropdown
+     */
+    const getShortestPath = async () => {
+      // only call shortest path endpoint if two players have been selected
+      if (player1 && player2) {
+        setLoadingPath(true);
+        const player1Id = player1.id;
+        const player2Id = player2.id;
+        const shortestPathResponse = await getShortestPathBetweenTwoPlayers(
+          player1Id,
+          player2Id
+        );
+        setShortestPath(shortestPathResponse);
+        setLoadingPath(false);
+      }
+    };
+
+    getShortestPath();
+  }, [player1, player2]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <PlayerSearchHeader
+        label1="Player 1 (Enter to Search)"
+        label2="Player 2 (Enter to Search)"
+        onSelect1={setPlayer1}
+        onSelect2={setPlayer2}
+      />
 
-export default App
+      <main
+        style={{
+          paddingTop: "100px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {loadingPath && <CircularProgress />}
+        {shortestPath ? (
+          <div>{JSON.stringify(shortestPath)}</div>
+        ) : (
+          <div>
+            Select two players from the dropdowns to see their degrees of
+            separation
+          </div>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default App;
