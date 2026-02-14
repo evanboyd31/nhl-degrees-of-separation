7baeb096ee59ec77da927d82f7601c073e9c60b6
Evan Boyd <evan.boyd@mail.mcgill.ca>
2026-02-13 21:42:06 -0500

    Update mapping to use logo_url as the key and a Single TeamPeriod as the value

--- frontend/src/constants/teamColors.ts
+++ frontend/src/constants/teamColors.ts
@@ -1,7 +1,43 @@
 import { type TeamPeriod } from "../types/nhl";
 
 export const TEAM_COLORS: Record<string, TeamPeriod> = {
-  "Quebec Nordiques": {
+  // Anaheim Ducks
+  "https://assets.nhle.com/logos/nhl/svg/ANA_19931994-19951996_light.svg": {
+    fullName: "Anaheim Ducks",
+    startYear: 1993,
+    endYear: 1996,
+    primaryColor: "#00685e",
+    secondaryColor: "#ffc72c",
+  },
+  "https://assets.nhle.com/logos/nhl/svg/ANA_19961997-20052006_light.svg": {
+    fullName: "Anaheim Ducks",
+    startYear: 1993,
+    endYear: 1996,
+    primaryColor: "#00685e",
+    secondaryColor: "#ffc72c",
+  },
+  "https://assets.nhle.com/logos/nhl/svg/ANA_20062007-20122013_light.svg": {
+    fullName: "Anaheim Ducks",
+    startYear: 2006,
+    endYear: 2013,
+    primaryColor: "#fc4c02",
+    secondaryColor: "#b9975b",
+  },
+  "https://assets.nhle.com/logos/nhl/svg/ANA_20132014-20232024_light.svg": {
+    fullName: "Anaheim Ducks",
+    startYear: 2013,
+    endYear: 2024,
+    primaryColor: "#fc4c02",
+    secondaryColor: "#b9975b",
+  },
+  "https://assets.nhle.com/logos/nhl/svg/ANA_light.svg": {
+    fullName: "Anaheim Ducks",
+    primaryColor: "#cf4520",
+    secondaryColor: "#89734c",
+  },
+
+  // Quebec Nordiques
+  "https://assets.nhle.com/logos/nhl/svg/QUE_19791980-19941995_light.svg": {
     fullName: "Quebec Nordiques",
     startYear: 1979,
     endYear: 1995,
