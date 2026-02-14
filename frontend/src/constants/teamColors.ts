ec7884067b5fb83fc0f6dfa6c9793435ab73e2b6
Evan Boyd <evan.boyd@mail.mcgill.ca>
2026-02-13 21:28:44 -0500

    Add Quebec Nordiques colors

---
+++ frontend/src/constants/teamColors.ts
@@ -0,0 +1,11 @@
+import { type TeamPeriod } from "../types/nhl";
+
+export const TEAM_COLORS: Record<string, TeamPeriod> = {
+  "Quebec Nordiques": {
+    fullName: "Quebec Nordiques",
+    startYear: 1979,
+    endYear: 1995,
+    primaryColor: "#0e7ad1",
+    secondaryColor: "#c8102e",
+  },
+};
