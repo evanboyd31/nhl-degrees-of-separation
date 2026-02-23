d527cac537f8e5550c5ac75a9dde1a2fee8d4e4c
Evan Boyd <evan.boyd@mail.mcgill.ca>
2026-02-23 17:32:36 -0500

    Add /players/random-shortest-path/ endpoint to players routes

--- backend/app/api/routes/players.py
+++ backend/app/api/routes/players.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from app.core.neo4j import get_session
-from app.services.players_service import get_player_by_search_string, get_shortest_path
+from app.services.players_service import get_player_by_search_string, get_shortest_path, get_random_shortest_path
 
 router = APIRouter(prefix="/players")
 
@@ -31,3 +31,14 @@ def find_shortest_path_between_two_players(player_1_id: int, player_2_id: int, s
   return get_shortest_path(session=session,
                            player_1_id=player_1_id,
                            player_2_id=player_2_id)
+
+@router.get("/random-shortest-path/")
+def find_shortest_path_between_two_random_players(session = Depends(get_session)):
+  """
+  The GET /random-shortest-path/ endpoint allows the user to search for an example 
+  shortest path between two randomized NHL players
+  
+  :param session: Neo4j DB Session dependency
+  """
+
+  return get_random_shortest_path(session=session)
